c3913e1d6c6977323042e1dae03bf5680b75096e
Irving Maldonado Olguin <irvingolguim@gmail.com>
2024-06-20 11:48:01 -0600

    Se agrego el archivo Maldonado.py
    Signed-off-by: Irving Maldonado Olguin <irvingolguim@gmail.com>

---
+++ Team2/Maldonado.py
@@ -0,0 +1,5 @@
+def clases_sorted(clases, frecuencias):
+    # Ordenar los datos basados en las clases
+    data = sorted(zip(clases, frecuencias))
+    list_c, list_FA = zip(*data)
+    return list_c, list_FA
