2b39b2ca55f4c5b11690b1525a8b5f796d65a388
Daniel Aldana Lopez <230110150@itsoeh.edu.mx>
2024-06-20 11:41:24 -0600

    funcion 1
    Signed-off-by: Daniel Aldana Lopez <230110150@itsoeh.edu.mx>

---
+++ Team2/Aldana.py
@@ -0,0 +1,17 @@
+def generar_clase_y_FABS(arr):
+    # Inicializar diccionario para contar frecuencias
+    frecuencia_dict = {}
+    
+    # Contar frecuencias de cada elemento
+    for item in arr:
+        if item in frecuencia_dict:
+            frecuencia_dict[item] += 1
+        else:
+            frecuencia_dict[item] = 1
+            
+    # Extraer clases y frecuencias en listas separadas
+    clases = list(frecuencia_dict.keys())
+    frecuencias = list(frecuencia_dict.values())
+    
+    return clases, frecuencias
+    
