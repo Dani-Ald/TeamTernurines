11412c005dd902b53bec818083f0895e0086bdb8
Irving Maldonado Olguin <irvingolguim@gmail.com>
2024-06-20 11:57:13 -0600

    Se agrego Maldonado.py
    Signed-off-by: Irving Maldonado Olguin <irvingolguim@gmail.com>

--- Team2/.ipynb_checkpoints/Maldonado-checkpoint.py
+++ Team2/.ipynb_checkpoints/Maldonado-checkpoint.py
@@ -2,4 +2,4 @@ def clases_sorted(clases, frecuencias):
     # Ordenar los datos basados en las clases
     data = sorted(zip(clases, frecuencias))
     list_c, list_FA = zip(*data)
-    return list_c, list_FA
+    return list_c, list_Fa
