91fc692090ca4c9a8ff61476a097cbc1b033365a
Daniel Aldana Lopez <230110150@itsoeh.edu.mx>
2024-06-26 23:34:18 -0600

    Hola

--- Caracteres/acumulada.py
+++
@@ -1,4 +0,0 @@
-def frecuencia_acumulada(frecuencias):
-    total_datos = sum(frecuencias)
-    frec_acumulada = [sum(frecuencias[:i+1]) / total_datos * 100 for i in range(len(frecuencias))]
-    return frec_acumulada
--- Caracteres/relativa.py
+++
@@ -1,3 +0,0 @@
-def frecuencia_relativa(frecuencias, total_datos):
-    frec_relativa = [frec / total_datos for frec in frecuencias]
-    return frec_relativa
